9735d57439c4705ba38cfe4e1f9527f83261315f
duboismax <duboismax01@gmail.com>
2023-06-16 12:58:05 +0200

    Avant Secteur

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
 use App\Entity\User;
 use App\Entity\Worker;
+use App\Form\MailerType;
 use Symfony\Component\Mime\Email;
 use App\Repository\WorkerRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,21 +17,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MailerController extends AbstractController
 {
-    #[Route('/email/{Slug}', name: 'email')]
-    public function sendEmail(MailerInterface $mailer, Worker $worker, WorkerRepository $workerRepository): Response
+    #[Route('/worker/{Slug}/email', name: 'worker_email')]
+    public function sendEmailWorker(MailerInterface $mailer, Worker $worker, EntityManagerInterface $manager, Request $request): Response
     {
-
         $creatorEmail = $worker->getUser()->getEmail();
         $user = $this->getUser();
         $email = (new Email())
-            ->from($user->getEmail())
+            ->from('noreply@wip.be')
             ->to($creatorEmail)
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
+            ->subject('Votre profil intéresse')
+            ->text('Bonjour ' .$worker->getUser()->getUsername(). ' un utilisateur est intéressé par votre profil de worker, vous pouvez le contacter sur cette adresse mail : ' .$user->getEmail())
             ->html('<p>See Twig integration for better HTML integration!</p>');
 
         $mailer->send($email);
@@ -37,11 +36,33 @@ class MailerController extends AbstractController
             'Mail Envoyé'
         );
 
+        return $this->render('worker/workerPartials.html.twig', [
+            "worker" => $worker
+        ]);    
+    }
+    
+    #[Route('/company/{Slug}/email', name: 'company_email')]
+    public function sendEmail(MailerInterface $mailer, Company $company, EntityManagerInterface $manager, Request $request): Response
+    {
+        $creatorEmail = $company->getUser()->getEmail();
+        $user = $this->getUser();
+        $email = (new Email())
+            ->from('noreply@wip.be')
+            ->to($creatorEmail)
+            ->subject('Votre profil intéresse')
+            ->text('Bonjour ' .$company->getUser()->getUsername(). ' un utilisateur est intéressé par votre company ' .$company->getName(). ', vous pouvez le contacter sur cette adresse mail : ' .$user->getEmail())
+            ->html('<p>Bonjour ' .$company->getUser()->getUsername(). ' un utilisateur est intéressé par votre company ' .$company->getName(). ', vous pouvez le contacter sur cette adresse mail : ' .$user->getEmail().' </p>');
 
+        $mailer->send($email);
 
-        // ...
-        return $this->render('/mailer/index.html.twig');
+        $this->addFlash(
+            'success',
+            'Mail Envoyé'
+        );
+
+        return $this->render('company/companyPartials.html.twig', [
+            "company" => $company
+        ]);
     }
     
 }
-
